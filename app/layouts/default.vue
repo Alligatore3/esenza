c04449e39769dac98825519786cf352043864856
Mattia Zanella <mattia_zanella@icloud.com>
2026-05-04 21:16:57 +0200

    Add <AppNotificationsStack

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,6 +3,7 @@
     class="relative flex min-h-screen w-full flex-col overflow-x-hidden bg-background-light dark:bg-background-dark text-text-main font-display antialiased"
   >
     <AppHeader />
+    <AppNotificationsStack />
     <main class="flex flex-col items-center w-full grow">
       <slot />
     </main>
