a110382feccdc542846a1c1716ededf46a9b1aaf
Mattia Zanella <mattia_zanella@icloud.com>
2026-06-04 18:33:10 +0200

    Replace images

--- app/components/how/HowHero.vue
+++ app/components/how/HowHero.vue
@@ -7,9 +7,11 @@ const localePath = useLocalePath()
   <section class="w-full relative min-h-[420px] flex items-end overflow-hidden">
     <div
       class="absolute inset-0 bg-cover bg-center"
-      style="background-image: url('/images/how-works.jpeg')"
+      style="background-image: url('https://www.datocms-assets.com/208615/1780386368-dsc_0443.jpg')"
     ></div>
-    <div class="absolute inset-0 bg-gradient-to-t from-background-dark/90 via-background-dark/50 to-transparent"></div>
+    <div
+      class="absolute inset-0 bg-gradient-to-t from-background-dark/90 via-background-dark/50 to-transparent"
+    ></div>
 
     <div class="relative z-10 w-full max-w-wide mx-auto px-4 md:px-10 lg:px-16 pb-16 pt-32">
       <h1 class="text-4xl md:text-6xl font-extrabold text-white leading-tight mb-3">
--- app/components/shop/ShopHero.vue
+++ app/components/shop/ShopHero.vue
@@ -6,9 +6,11 @@ const { t } = useI18n()
   <section class="w-full relative min-h-[360px] flex items-end overflow-hidden">
     <div
       class="absolute inset-0 bg-cover bg-center"
-      style="background-image: url('/images/shop.jpeg')"
+      style="background-image: url('https://www.datocms-assets.com/208615/1780386369-dsc_0440.jpg')"
     />
-    <div class="absolute inset-0 bg-gradient-to-t from-background-dark/90 via-background-dark/50 to-transparent"></div>
+    <div
+      class="absolute inset-0 bg-gradient-to-t from-background-dark/90 via-background-dark/50 to-transparent"
+    ></div>
 
     <div class="relative z-10 w-full max-w-wide mx-auto px-4 md:px-10 lg:px-16 pb-12 pt-28">
       <h1 class="text-4xl md:text-5xl font-extrabold text-white leading-tight mb-2">
