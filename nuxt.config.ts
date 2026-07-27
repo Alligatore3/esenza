0c06ac52c843463d000de92803b116dcd207580c
Mattia Zanella <mattia_zanella@icloud.com>
2026-07-27 16:18:18 +0100

    Fix SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       htmlAttrs: { lang: 'en' },
-      title: 'èSenza Japan — Italian Organic Mixes',
+      title: 'èSenza — Italian Organic Mixes',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
